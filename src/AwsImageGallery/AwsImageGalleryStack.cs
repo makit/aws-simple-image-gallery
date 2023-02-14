480ec1fdd60629f8b2f86997937693b5291e0fb9
makit <info@makit.net>
2023-02-14 19:41:04 +0000

    Initial commit

---
+++ src/AwsImageGallery/AwsImageGalleryStack.cs
@@ -0,0 +1,13 @@
+using Amazon.CDK;
+using Constructs;
+
+namespace AwsImageGallery
+{
+    public class AwsImageGalleryStack : Stack
+    {
+        internal AwsImageGalleryStack(Construct scope, string id, IStackProps props = null) : base(scope, id, props)
+        {
+            // The code that defines your stack goes here
+        }
+    }
+}
